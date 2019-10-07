033c72d136ff07cd66a442045c2e4b235a292cf4
Pankaj-bhoi <pankajcool.bhoi@gmail.com>
2019-10-08 00:30:02 +0530

    Completed..

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    //username = users
+    //password = users1234
+    mongoURI: "mongodb://users:users1234@ds129098.mlab.com:29098/vote_project"
+    //mongoURI:"mongodb+srv://Votinguser:Rudra@890@cluster0-t55cz.mongodb.net/admin?retryWrites=true&w=majority"
+
+}
